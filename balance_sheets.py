c8ab19317ffb2249aa5d505fcdf07f35373575d2
erict <edtufte@crimson.ua.edu>
2022-06-24 03:22:37 +0000

    running into column count error

--- balance_sheets.py
+++ balance_sheets.py
@@ -1,3 +1,5 @@
+### TODO: correct column count error
+
 import yahoo_fin.stock_info as si
 import pandas as pd
 import numpy as np
@@ -13,11 +15,11 @@ def pull_fin_info(function_call, stock_list):
     results_df = {}
     for ticker in stock_list:
         try:
-            results_df[ticker] = futures_df[ticker].result(12) # 60 sec timeout
-            break
+            results_df[ticker] = futures_df[ticker].result(60) # 60 sec timeout
         except:
             print('error on ' + ticker)
-
+            break
+    
     recent_sheets = pd.DataFrame(np.empty((0, 1)))
     recent_sheets = {ticker : sheet.iloc[:,:1] for ticker,sheet in results_df.items()}
     for ticker in recent_sheets.keys():
@@ -28,30 +30,26 @@ def pull_fin_info(function_call, stock_list):
     recent_sheets.columns = ['Ticker', 'Breakdown', 'Recent']
     return recent_sheets
 
-startTime = time.time()
 
+startTime = time.time()
 print('timer started seconds - ' + str(round(time.time() - startTime,2)))
 
 stock_list = ['AMZN','KO','TSLA','GME','AAPL','GOOG','SPOT']
 
-# stock_list.extend(si.tickers_ftse100())
+stock_list.extend(si.tickers_ftse100())
 # stock_list.extend(si.tickers_sp500())
-
-stock_list = list(dict.fromkeys(stock_list))
-print(stock_list)
-
 print('stock list pull done seconds - ' + str(round(time.time() - startTime,2)))
 
-balance_sheets = pull_fin_info(si.get_balance_sheet, stock_list)
+stock_list = list(dict.fromkeys(stock_list))
+# print(stock_list)
 
+balance_sheets = pull_fin_info(si.get_balance_sheet, stock_list)
 print('balance sheets done seconds - ' + str(round(time.time() - startTime,2)))
 
 income_statements = pull_fin_info(si.get_income_statement, stock_list)
-
 print('income statements done seconds - ' + str(round(time.time() - startTime,2)))
 
 cash_flow_statements = pull_fin_info(si.get_cash_flow, stock_list)
-
 print('cash flow statements done seconds - ' + str(round(time.time() - startTime,2)))
 
 frames = [balance_sheets, income_statements, cash_flow_statements]
