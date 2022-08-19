4b5ea4decf226f87d35cbcea485323e0cafd3a63
Eric Tufte <edtufte@crimson.ua.edu>
2022-08-18 21:35:56 -0500

    wrap program in function

--- balance_sheets.py
+++ balance_sheets.py
@@ -34,39 +34,42 @@ def pull_fin_info(function_call, stock_list):
     recent_sheets.columns = ['Ticker', 'Breakdown', 'Recent']
     return recent_sheets
 
+def main_program():
+    startTime = time.time()
+    print('timer started seconds - ' + str(round(time.time() - startTime,2)))
 
-startTime = time.time()
-print('timer started seconds - ' + str(round(time.time() - startTime,2)))
+    stock_list = ['AMZN','KO','TSLA','GME','AAPL','GOOG','SPOT']
+    stock_list.extend(['ELV'])
 
-stock_list = ['AMZN','KO','TSLA','GME','AAPL','GOOG','SPOT']
-stock_list.extend(['ELV'])
+    stock_list.extend(si.tickers_dow())
+    # stock_list.extend(si.tickers_sp500())
+    print('stock list pull done seconds - ' + str(round(time.time() - startTime,2)))
 
-stock_list.extend(si.tickers_dow())
-# stock_list.extend(si.tickers_sp500())
-print('stock list pull done seconds - ' + str(round(time.time() - startTime,2)))
+    stock_list = list(dict.fromkeys(stock_list))
+    # print(stock_list)
 
-stock_list = list(dict.fromkeys(stock_list))
-# print(stock_list)
+    balance_sheets = pull_fin_info(si.get_balance_sheet, stock_list)
+    print('balance sheets done seconds - ' + str(round(time.time() - startTime,2)))
 
-balance_sheets = pull_fin_info(si.get_balance_sheet, stock_list)
-print('balance sheets done seconds - ' + str(round(time.time() - startTime,2)))
+    income_statements = pull_fin_info(si.get_income_statement, stock_list)
+    print('income statements done seconds - ' + str(round(time.time() - startTime,2)))
 
-income_statements = pull_fin_info(si.get_income_statement, stock_list)
-print('income statements done seconds - ' + str(round(time.time() - startTime,2)))
+    cash_flow_statements = pull_fin_info(si.get_cash_flow, stock_list)
+    print('cash flow statements done seconds - ' + str(round(time.time() - startTime,2)))
 
-cash_flow_statements = pull_fin_info(si.get_cash_flow, stock_list)
-print('cash flow statements done seconds - ' + str(round(time.time() - startTime,2)))
+    frames = [balance_sheets, income_statements, cash_flow_statements]
+    combined_df = pd.concat(frames)
+    combined_df = combined_df.reset_index()
 
-frames = [balance_sheets, income_statements, cash_flow_statements]
-combined_df = pd.concat(frames)
-combined_df = combined_df.reset_index()
+    pivot_df = combined_df.pivot_table(index='Ticker', columns='Breakdown', values='Recent', aggfunc='sum')
 
-pivot_df = combined_df.pivot_table(index='Ticker', columns='Breakdown', values='Recent', aggfunc='sum')
+    print(pivot_df.query("Ticker == 'AMZN'"))
 
-print(pivot_df.query("Ticker == 'AMZN'"))
+    print(pivot_df.query("ebit >= 0"))
 
-print(pivot_df.query("ebit >= 0"))
+    print(pivot_df.head())
 
-print(pivot_df.head())
+    print('Execution time in seconds: ' + str(round(time.time() - startTime,2)))
 
-print('Execution time in seconds: ' + str(round(time.time() - startTime,2)))
+if __name__ == '__main__':
+    main_program()
